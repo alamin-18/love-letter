f5fcc27d3bdcd86e651c6cf5813c3f3564ea72b8
Md Al Amin <fpalamin6@gmail.com>
2022-08-15 12:06:39 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const btn1 = document.getElementById('btn-1');
+btn1.addEventListener('click', function(){
+    document.getElementById('text').innerText="I love you! 💕";
+});
+
+const btn2 = document.getElementById('btn-2');
+btn2.addEventListener('click', function(){
+    document.getElementById('text').innerText="batter then next time! 💕";
+})
